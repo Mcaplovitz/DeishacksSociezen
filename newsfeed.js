d8859e419d47456eac13cbd18bb2ffaa4b4c4c7c
fritztheritz <fduverglas@brandeis.edu>
2021-03-19 18:34:51 -0400

    redesign newsfeed.html, add js file with news api

---
+++ newsfeed.js
@@ -0,0 +1,8 @@
+var url = 'https://newsapi.org/v2/top-headlines?' +
+          'country=us&category=sports' +
+          'apiKey=1aa37b27121346e9b6b459f1c10e3ee7';
+var req = new Request(url);
+fetch(req)
+    .then(function(response) {
+        console.log(response.json());
+    })
