5f7af1850379ec5a14c472ddf900acac00fd49f8
Matthew Caplovitz <mcap@brandeis.edu>
2021-03-19 13:19:31 -0400

    Changed map to searchable
    Co-authored-by: Fritz-Gerald Duverglas <fritztheritz@users.noreply.github.com>

--- map.js
+++ map.js
@@ -1,63 +1,103 @@
 function initMap() {
-    var markers = [];
-    var marker = new google.maps.Marker();
-    var count = 0;
-    // The location of boston
-    const boston = {
-        lat: 42.36,
-        lng: -71.062500
-    };
-
-    // function findMarker() {
-    //     map.addListener('dblclick', function(event) {
-    //             deleteMarker = event.currentTarget,
-    //                 labler = deleteMarker.label,
-    //                     for (i = 0, i < markers.length, i++) {
-    //                         if (markers[i].label == labler) {
-    //                             markers[i].setMap(null);
-    //                         }
-    //                     }
-    //         )
-    //     };
-
-
-    // The map, centered at boston
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15,
-        center: boston,
+        mapTypeControl: false,
+        center: {
+            lat: 42.36,
+            lng: -71.062500
+        },
+        zoom: 13,
     });
+    new AutocompleteDirectionsHandler(map);
+}
 
-    map.addListener('click', function(event) {
-        marker = addMarker(event);
-    });
-
-    //**does not work yet**
-    marker.addListener('dblclick', function() {
-        marker.setMap(null);
-        var remover = markers.findIndex(marker);
-        // you should do something like *marker that needs to be removed*.setMap(null)
-        // but i'm also assuming that is what market.addListener is meant to do
-        // i'll keep looking around tho and see
-        remover.setMap(null);
-        markers.splice(remover);
-        alert("Working!");
-    });
-
-    function addMarker(event) {
-        var latitude = event.latLng.lat();
-        var longitude = event.latLng.lng();
-        var myLatlng = {
-            lat: latitude,
-            lng: longitude
-        };
-        var newMarker = new google.maps.Marker({
-            position: myLatlng,
-            map: map,
-            label: count++,
+class AutocompleteDirectionsHandler {
+    constructor(map) {
+            this.map = map;
+            this.originPlaceId = "";
+            this.destinationPlaceId = "";
+            this.travelMode = google.maps.TravelMode.WALKING;
+            this.directionsService = new google.maps.DirectionsService();
+            this.directionsRenderer = new google.maps.DirectionsRenderer();
+            this.directionsRenderer.setMap(map);
+            const originInput = document.getElementById("origin-input");
+            const destinationInput = document.getElementById("destination-input");
+            const modeSelector = document.getElementById("mode-selector");
+            const originAutocomplete = new google.maps.places.Autocomplete(originInput);
+            // Specify just the place data fields that you need.
+            originAutocomplete.setFields(["place_id"]);
+            const destinationAutocomplete = new google.maps.places.Autocomplete(
+                destinationInput
+            );
+            // Specify just the place data fields that you need.
+            destinationAutocomplete.setFields(["place_id"]);
+            this.setupClickListener(
+                "changemode-walking",
+                google.maps.TravelMode.WALKING
+            );
+            this.setupClickListener(
+                "changemode-transit",
+                google.maps.TravelMode.TRANSIT
+            );
+            this.setupClickListener(
+                "changemode-driving",
+                google.maps.TravelMode.DRIVING
+            );
+            this.setupClickListener(
+                "changemode-biking",
+                google.maps.TravelMode.BIKING
+            );
+            this.setupPlaceChangedListener(originAutocomplete, "ORIG");
+            this.setupPlaceChangedListener(destinationAutocomplete, "DEST");
+            this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+            this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(
+                destinationInput
+            );
+            this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
+        }
+        // Sets a listener on a radio button to change the filter type on Places
+        // Autocomplete.
+    setupClickListener(id, mode) {
+        const radioButton = document.getElementById(id);
+        radioButton.addEventListener("click", () => {
+            this.travelMode = mode;
+            this.route();
         });
-        //put markers into the array
-        markers.push(newMarker);
-        return newMarker;
-        //console.log(markers);
+    }
+    setupPlaceChangedListener(autocomplete, mode) {
+        autocomplete.bindTo("bounds", this.map);
+        autocomplete.addListener("place_changed", () => {
+            const place = autocomplete.getPlace();
+
+            if (!place.place_id) {
+                window.alert("Please select an option from the dropdown list.");
+                return;
+            }
+
+            if (mode === "ORIG") {
+                this.originPlaceId = place.place_id;
+            } else {
+                this.destinationPlaceId = place.place_id;
+            }
+            this.route();
+        });
+    }
+    route() {
+        if (!this.originPlaceId || !this.destinationPlaceId) {
+            return;
+        }
+        const me = this;
+        this.directionsService.route({
+                origin: { placeId: this.originPlaceId },
+                destination: { placeId: this.destinationPlaceId },
+                travelMode: this.travelMode,
+            },
+            (response, status) => {
+                if (status === "OK") {
+                    me.directionsRenderer.setDirections(response);
+                } else {
+                    window.alert("Directions request failed due to " + status);
+                }
+            }
+        );
     }
 }
