04880f15aaa84313b37ca0da9aba2770dfe09820
fritztheritz <fduverglas@brandeis.edu>
2021-03-19 23:20:37 -0400

    finalized a look for the news feed page

--- newsfeed.js
+++ newsfeed.js
@@ -1,8 +1,10 @@
 var url = 'https://newsapi.org/v2/top-headlines?' +
           'country=us&category=sports' +
           'apiKey=1aa37b27121346e9b6b459f1c10e3ee7';
-var req = new Request(url);
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
-    })
+//var req = new Request(url);
+var response = requests.get(url);
+console.log(response);
+// fetch(req)
+//     .then(function(response) {
+//         console.log(response.json());
+//     })
