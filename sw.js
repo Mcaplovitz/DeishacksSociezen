78d8d47f894f241524b3dd1d2532f4308e369c9b
JohnLervandal <JohnLervandal@brandeis.edu>
2021-03-31 21:26:24 -0400

    update

--- sw.js
+++ sw.js
@@ -1,45 +1,77 @@
-var APP_PREFIX = 'Mouthful'     // Identifier for this app (this needs to be consistent across every cache update)
-var VERSION = 'version_01'              // Version of the off-line cache (change this value everytime you want to update cache)
-var CACHE_NAME = APP_PREFIX + VERSION
-var URLS = [                            // Add URL you want to cache in this list.
-  '/Mouthful/',                     // If you have separate JS/CSS files,
-  '/Mouthful/index.html'            // add path to those files here
-]
+self.addEventListener('install', event => {
+  console.log('Service Worker install', event);
 
-// Respond with cached resources
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
+  event.waitUntil((async () => {
+    const cache = await caches.open('static_files')
 
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
-    })
-  )
+    return cache.addAll(STATIC_FILES)
+  })())
 })
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil((async () => {
-    const keyList = await caches.keys();
-    await Promise.all(keyList.map((key) => {
-      if (key === cacheName) { return; }
-      await caches.delete(key);
-    }))
-  })());
-});
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated', event);
 
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
-  e.waitUntil((async () => {
-    const cache = await caches.open(cacheName);
-    console.log('[Service Worker] Caching all: app shell and content');
-    await cache.addAll(contentToCache);
-  })());
-});
+  event.waitUntil((async () => {
+    return event.waitUntil(self.clients.claim());
+  })())
+})
+
+self.addEventListener('fetch', (event) => {
+  /**
+   * Cache Strategy Cache only
+   */
+  if (arrayIncludes(event.request.url, STATIC_FILES) === true) {
+    event.respondWith(caches.match(event.request))
+
+    return
+  }
+
+  /**
+   * Cache Strategy Network first, Cache fallback
+   */
+  if (event.request.headers.get('accept').includes('application/json')) {
+    event.respondWith((async () => {
+      try {
+        const response = await fetch(event.request)
+        const cache = await caches.open('dynamic_files')
+
+        cache.put(event.request.url, response.clone())
+        console.log('Return from fetch %s', event.request.url)
+
+        return response
+
+      } catch (error) {
+        const cached = await caches.match(event.request)
+
+        if (cached) {
+          console.log('Return from cache %s', event.request.url)
+          return cached
+        }
+
+        throw error
+      }
+    })())
+
+    return
+  }
+
+  /**
+   * Cache Strategy Cache first, Network fallback
+   */
+  if (event.request.method === 'GET') {
+    // fetch dynamic files the first time and return the second time from the cache
+    event.respondWith((async () => {
+      const response = await caches.match(event.request)
+
+      if (response) return response;
+
+      console.log('Dynamic Caching: %s', event.request.url)
+      const result = await fetch(event.request)
+      const cache = await caches.open('dynamic_files')
+
+      cache.put(event.request.url, result.clone())
+
+      return result
+    })())
+  }
+})
