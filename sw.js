e101105a2f880c6cacb4e140c0f7694f21845e75
JohnLervandal <JohnLervandal@brandeis.edu>
2021-03-31 20:48:37 -0400

    lets hope it doesn't break

--- sw.js
+++ sw.js
@@ -37,15 +37,31 @@ self.addEventListener('activate', (event) => {
   })());
 });
 
-function update(request) {
-    return caches.open(CACHE).then(function (cache) {
-      return fetch(request).then(function (response) {
-        return cache.put(request, response.clone()).then(function () {
-          return response;
-        });
-      });
-    });
+var networkDataReceived = false;
+
+startSpinner();
+
+// fetch fresh data
+var networkUpdate = fetch('/data.json').then(function(response) {
+  return response.json();
+}).then(function(data) {
+  networkDataReceived = true;
+  updatePage(data);
+});
+
+// fetch cached data
+caches.match('/data.json').then(function(response) {
+  if (!response) throw Error("No data");
+  return response.json();
+}).then(function(data) {
+  // don't overwrite newer network data
+  if (!networkDataReceived) {
+    updatePage(data);
   }
+}).catch(function() {
+  // we didn't get cached data, the network is our last hope:
+  return networkUpdate;
+}).catch(showErrorMessage).then(stopSpinner());
 
 // Cache resources
 self.addEventListener('install', function (e) {
